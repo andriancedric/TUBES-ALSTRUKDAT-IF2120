3a1deabbaba92b00239d5502134a9f8681b4bcc7
anakduaribu <42271632+anakduaribu@users.noreply.github.com>
2018-11-13 15:41:43 +0700

    Create modstack.c

---
+++ modstack.c
@@ -0,0 +1,89 @@
+
+#include <stdio.h>
+#include "stacktray.h"
+
+/* ************ Prototype ************ */
+/* *** Konstruktor/Kreator *** */
+void CreateEmpty (Stack *S) /*Bisa dipanggil saat CH untuk membuang Hand, CT untuk tray*/
+/* I.S. sembarang; */
+/* F.S. Membuat sebuah stack S yang kosong berkapasitas MaxEl */
+/* jadi indeksnya antara 1.. MaxEl+1 karena 0 tidak dipakai */
+/* Ciri stack kosong : TOP bernilai Nil */
+{
+  //KAMUS
+  //ALGORITMA
+  Top(*S) = Nil;
+}
+
+/* ************ Predikat Untuk test keadaan KOLEKSI ************ */
+boolean IsEmpty (Stack S)
+/* Mengirim true jika Stack kosong: lihat definisi di atas */
+{
+  //KAMUS
+  //ALGORITMA
+  return (Top(S) == Nil);
+}
+boolean IsFull (Stack S)
+/* Mengirim true jika tabel penampung nilai elemen stack penuh */
+{
+  //KAMUS
+  //ALGORITMA
+  return (Top(S) == MaxEl);
+}
+
+/* ************ Menambahkan sebuah elemen ke Stack ************ */
+void Push (Stack *S, infotype X)
+/* Menambahkan X sebagai elemen Stack S. */
+/* I.S. S mungkin kosong, tabel penampung elemen stack TIDAK penuh */
+/* F.S. X menjadi TOP yang baru,TOP bertambah 1 */
+{
+  //KAMUS
+  //ALGORITMA
+    Top(*S)++;
+    InfoTop(*S) = X;
+}
+
+/* ************ Menghapus sebuah elemen Stack ************ */
+void Pop (Stack *S, infotype *X)
+/* Menghapus X dari Stack S. */
+/* I.S. S  tidak mungkin kosong */
+/* F.S. X adalah nilai elemen TOP yang lama, TOP berkurang 1 */
+{
+  //KAMUS
+  //ALGORITMA
+  *X = InfoTop(*S);
+  Top(*S)--;
+}
+
+void Put (Stack *SH, Stack *ST){ /*SH adalah stack hand, ST adalah Stack tray*/
+	/*I.S. : Ada tumpukan makanan di tangan, terbentuk karena urutan*/
+	/*F.S. : Makanan yang terbentuk dipindah ke tray*/
+
+	//KAMUS
+	infotype MakananTemp;
+
+	//ALGORITMA
+	if (IsFull(ST)){ /*tray penuh*/
+		printf("Tray sudah penuh");
+	} 
+	else {
+		Pop(SH,&MakananTemp);
+		Push(ST, MakananTemp);
+	}
+}
+
+void Take (infotype ID, Stack *SH) { 
+    /*I.S. : Ada bahan di sebelah pemain, kondisi tangan tidak penuh*/
+	/*F.S. : Bahan berpindah ke tangan*/
+
+    //KAMUS
+    infotype IDBahan;
+
+    //ALGORITMA
+    if(!IsEmpty(SH)){
+        Push(SH, IdBahan);
+    }
+    else{
+        print('Tangan kamu penuh. Kosongi dahulu');
+    }
+}
